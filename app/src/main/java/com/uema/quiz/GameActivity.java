2c8695df450e5da4d2fe36a7f50e45d877311193
cicero guilherme <ciceroguilhermepereira@gmail.com>
2019-06-29 18:04:59 -0300

    Activity Resposta Atualizado

--- app/src/main/java/com/uema/quiz/GameActivity.java
+++ app/src/main/java/com/uema/quiz/GameActivity.java
@@ -1,8 +1,11 @@
 package com.uema.quiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //Troca de telas
+        Button btnResposta = findViewById(R.id.btnNext);
+
+        btnResposta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentNext = new Intent(GameActivity.this, RespostaActivity.class);
+
+                startActivity(intentNext);
+            }
+        });
     }
 }
--- app/src/main/java/com/uema/quiz/RespostaActivity.java
+++ app/src/main/java/com/uema/quiz/RespostaActivity.java
@@ -8,5 +8,6 @@ public class RespostaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resposta);
     }
 }
