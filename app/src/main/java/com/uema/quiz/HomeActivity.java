c90d291174e50034975589ad93fb13bdb8b5e7cd
cicero guilherme <ciceroguilhermepereira@gmail.com>
2019-06-29 12:18:21 -0300

    Criacao da Activity Game

--- app/src/main/java/com/uema/quiz/HomeActivity.java
+++ app/src/main/java/com/uema/quiz/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.uema.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Troca de telas
+        Button btnJogar = findViewById(R.id.btnJogar);
+
+        btnJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentGame = new Intent(HomeActivity.this, GameActivity.class);
+
+                startActivity(intentGame);
+            }
+        });
+
     }
 }
