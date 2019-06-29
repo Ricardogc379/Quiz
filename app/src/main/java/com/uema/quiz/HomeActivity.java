ebd7456b042adc0c417cfd6fc3bd51292c35e65c
Ricardo <ricardogc379@gmail.com>
2019-06-29 16:12:34 -0300

    Resolucao do conflito

---
+++ app/src/main/java/com/uema/quiz/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.uema.quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Troca de telas
+        Button btnJogar = findViewById(R.id.btnJogar);
+
+        btnJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentGame = new Intent(HomeActivity.this, GameActivity.class);
+
+                startActivity(intentGame);
+            }
+        });
+
+    }
+}
