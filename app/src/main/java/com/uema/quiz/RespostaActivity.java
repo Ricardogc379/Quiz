cb14243f850a2b1f6faa4b3e6abe70aec0169a8a
cicero guilherme <ciceroguilhermepereira@gmail.com>
2019-06-30 12:41:22 -0300

    Activity Resposta implementacao

--- app/src/main/java/com/uema/quiz/RespostaActivity.java
+++ app/src/main/java/com/uema/quiz/RespostaActivity.java
@@ -1,5 +1,6 @@
 package com.uema.quiz;
 
+import android.os.Trace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,22 @@ public class RespostaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                finish();
+            }
+        });
+
+        thread.start();
+
+
     }
 }
